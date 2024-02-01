4e860454e1eb2f9099236468b3b7d402079dd7d8
Val Song <valsong@163.com>
2024-02-01 17:38:17 +0800

    7.17.17

--- bin/install-logstash-core.sh
+++ bin/install-logstash-core.sh
@@ -1,2 +1,2 @@
 # install logstash-core.jar to local repository
-mvn install:install-file -Dfile=./logstash-core.jar -DgroupId=org.logstash -DartifactId=logstash-core -Dversion=6.8.13 -Dpackaging=jar
+mvn install:install-file -Dfile=./logstash-core.jar -DgroupId=org.logstash -DartifactId=logstash-core -Dversion=7.17.17 -Dpackaging=jar
